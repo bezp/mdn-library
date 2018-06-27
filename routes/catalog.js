57e2fb8fa8ebf2b997a93c3eb5e3cc1cc9925efa
bezalel <bezalelp@uci.edu>
2018-06-27 15:54:49 -0400

    eh

--- routes/catalog.js
+++ routes/catalog.js
@@ -18,7 +18,7 @@ router.get('/book/create', book_controller.book_create_get);
 // POST request for creating Book.
 router.post('/book/create', book_controller.book_create_post);
 
-// GET request to delete Book.
+// GET request to delete Book. 
 router.get('/book/:id/delete', book_controller.book_delete_get);
 
 // POST request to delete Book.
